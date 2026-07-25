2c75aefb5846e2b20dbd9ace60f2a45c86cf8d3a
MUNDUS3684 <suteemonkaikai2019@gmail.com>
2026-07-25 10:49:23 +0700

    Play just 2 balls

--- Assets/Script/Bowling.cs
+++ Assets/Script/Bowling.cs
@@ -1,16 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Bowling : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private int forcePower;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.spaceKey.isPressed)
+            Shootball();
+    }
+
+    private void Shootball()
+    {
+        rb.AddForce(Vector3.forward * forcePower, ForceMode.Impulse);  //พวกที่เป็น x z y คือพวก forward up down
     }
 }
